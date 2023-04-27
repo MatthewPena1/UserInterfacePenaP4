82002b2d940467491d9b10f4f982b65dc7a48361
MatthewPena1 <724514@student.dusd.net>
2023-04-27 11:37:19 -0700

    Worked on Unit 5 project
    Added a canvas/textbox; began the programming on keeping score

--- UserInterfacePena/Assets/Scripts/Target.cs
+++ UserInterfacePena/Assets/Scripts/Target.cs
@@ -8,8 +8,9 @@ public class Target : MonoBehaviour
     private float minSpeed = 12;
     private float maxSpeed = 16;
     private float maxTorque = 10;
-    private float xRange = 4;
+    private float xRange = 3;
     private float ySpawnPos = -6;
+    private GameManager gameManager;
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +19,7 @@ public class Target : MonoBehaviour
         targetRb.AddForce(RandomForce(), ForceMode.Impulse);
         targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
         transform.position = RandomSpawnPos();
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
     Vector3 RandomForce()
@@ -38,6 +40,7 @@ public class Target : MonoBehaviour
     private void OnMouseDown()
     {
         Destroy(gameObject);
+        gameManager.UpdateScore(5);
     }
 
     private void OnTriggerEnter(Collider other)
